26858184c8c6d5bb88d60ffa28a9d7e5d0073d22
leechris13 <lee.chris.sh@gmail.com>
2022-11-28 19:32:35 -0800

    accessing and dom-crud are done

--- accessing.js
+++ accessing.js
@@ -1,5 +1,16 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+const headLine = document.getElementById('weather-head')
+
+headLine.innerText = 'February 10 Weather Forecast, Seattle'
 
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+const sunEls = document.querySelectorAll('.sun')
+
+sunEls.forEach((el) => el.style.color = 'orange')
+
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+const list = document.getElementsByTagName('li')
+
+list[2].innerHTML = 'cloudy'
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,29 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
 
+const attri = document.createElement("a");
+const node = document.createTextNode("Buy Now!");
+attri.appendChild(node)
+
+attri.id = 'cta';
+
+const mainEl = document.getElementsByTagName('main')[0]
+mainEl.appendChild(attri)
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+const carImag = document.getElementsByTagName('img')[0];
+const color = carImag.getAttribute("data-color");
+console.log(color)
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const list = document.getElementsByTagName("li")
+list[2].className = "highlight"
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+const para = document.getElementsByTagName("p")
+let removedEl = mainEl.removeChild(para[0])
